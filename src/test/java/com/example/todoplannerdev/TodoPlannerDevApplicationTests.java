91af570c8ac952a7afacc859edb84558aea8bb60
ParkJin0814 <114732394+ParkJin0814@users.noreply.github.com>
2025-05-22 10:38:32 +0900

    first commit

---
+++ src/test/java/com/example/todoplannerdev/TodoPlannerDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todoplannerdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoPlannerDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
